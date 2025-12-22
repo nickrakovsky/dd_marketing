9a2dde60785d2342d95c3105fc8dcc2c0c5f70e8
jeowy <jeowy@hotmail.co.uk>
2025-12-22 13:43:29 +0000

    Switch hosting to Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,17 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   // ADD THIS LINE:
-  site: 'https://datadocks.com', 
+  site: 'https://datadocks.com',
 
   integrations: [
     tailwind(), 
     react()
-  ]
+  ],
+
+  adapter: vercel()
 });
