a536f76ff4d3a18dade962485d777bdaca1ab5f3
jeowy <jeowy@hotmail.co.uk>
2026-01-14 12:05:04 +0100

    reverting to stable

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,12 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  // PRODUCTION CONFIGURATION:
-  site: 'https://datadocks.com',
-  base: '/',
-  //TEST CONFIGURATION FOR GITHUB PAGES:
-  //site: 'https://nickrakovsky.github.io',
-  //base: '/dd_marketing',
+  // "PRODUCTION" CONFIGURATION BREAKS THE SITE::
+  //site: 'https://datadocks.com',
+  //base: '/',
+  //GITHUB PAGES CONFIGURATION:
+  site: 'https://nickrakovsky.github.io',
+  base: '/dd_marketing',
 
   integrations: [
     tailwind(), 
