b9f8bb989e9097c533388aee05dd8a3c793f52ab
jeowy <jeowy@hotmail.co.uk>
2026-01-20 12:06:47 +0100

    sitemap, favicon, etc

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   // "PRODUCTION" CONFIGURATION BREAKS THE SITE::
@@ -11,8 +13,18 @@ export default defineConfig({
   site: 'https://nickrakovsky.github.io',
   base: '/dd_marketing',
 
-  integrations: [
-    tailwind(), 
-    react()
+  integrations: [tailwind(), 
+    react(), 
+   sitemap({
+      // 2. THE FIX: Manually rewrite URLs for the sitemap only
+      serialize(item) {
+        // Replace the GitHub URL with the Production URL
+        item.url = item.url.replace(
+          'https://nickrakovsky.github.io/dd_marketing', 
+          'https://datadocks.com'
+        );
+        return item;
+      }
+    })
   ],
 });
