2310c909933b396ba014e5722d6896f86d4eca06
jeowy <jeowy@hotmail.co.uk>
2026-04-23 12:25:30 +0200

    resolve ambiguous tailwind easing warning by defining 'expo' curve in config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -71,6 +71,7 @@ export default {
       },
       transitionTimingFunction: {
         'smooth': 'cubic-bezier(0.4, 0, 0.2, 1)',
+        'expo': 'cubic-bezier(0.19, 1, 0.22, 1)',
       },
       borderRadius: {
         lg: "var(--radius)",
